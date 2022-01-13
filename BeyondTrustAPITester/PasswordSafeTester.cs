dfebfe546b294fefdd8b583e4a8972d775dfd8c3
Matthew Dobrowsky <matthew.dobrowsky@keyfactor.com>
2022-01-13 14:02:22 -0500

    add debug runner

---
+++ BeyondTrustAPITester/PasswordSafeTester.cs
@@ -0,0 +1,34 @@
+﻿using Keyfactor.Extensions.Pam.BeyondInsight.PasswordSafe;
+using System;
+using System.Collections.Generic;
+
+namespace BeyondTrustAPITester
+{
+    class PasswordSafeTester
+    {
+        static string USERNAME = "beyondtrust";
+        static string PASSWORD = "###";
+        static string APIUSER = "apiuser";
+        static string APIKEY = "###";
+        static string URL = "https://###/BeyondTrust/api/public/v3/";
+
+        static void Main(string[] args)
+        {
+            var client = new PasswordSafePAM();
+            var initVars = new Dictionary<string, string>()
+            {
+                { "Host", URL },
+                { "APIKey", APIKEY },
+                { "Username", USERNAME }
+            };
+
+            var instanceVars = new Dictionary<string, string>()
+            {
+                { "SystemID", "1" },
+                { "AccountID", "1" }
+            };
+
+            client.GetPassword(instanceVars, initVars);
+        }
+    }
+}
