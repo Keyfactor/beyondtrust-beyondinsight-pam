6b6f7804b4e7cade0144d917311558a33f1f5231
Matthew Dobrowsky <matthew.dobrowsky@keyfactor.com>
2022-01-18 22:26:25 -0500

    add sql installation script for PAM provider type

---
+++ PasswordSafe/add_PAMProvider.sql
@@ -0,0 +1,13 @@
+﻿declare @pamid uniqueidentifier;
+set @pamid = newid();
+
+insert into [pam].[ProviderTypes]([Id], [Name])
+values (@pamid, 'BeyondTrust PasswordSafe');
+
+insert into [pam].[ProviderTypeParams]([ProviderTypeId], [Name], [DisplayName], [DataType], [InstanceLevel])
+values	(@pamid, 'Host', 'BeyondTrust Host', 1, 0),
+		(@pamid, 'APIKey', 'BeyondTrust API Key', 1, 0),
+		(@pamid, 'Username', 'BeyondTrust Username', 1, 0),
+		(@pamid, 'SystemID', 'BeyondTrust System ID', 1, 1),
+		(@pamid, 'AccountID', 'BeyondTrust Account ID', 1, 1);
+
