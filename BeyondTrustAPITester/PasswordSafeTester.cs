1c6b205102736bb9f1e54075490d569a51341e1d
Mikey Henderson <Michael.Henderson@keyfactor.com>
2023-05-24 16:50:38 -0700

    Release 1.1 (#14)
    * Add pam_types definition (#11)
    
    * update readme files for kfutil usage
    
    * match assembly name to repo name
    
    * Update CHANGELOG.md (#13)

--- BeyondTrustAPITester/PasswordSafeTester.cs
+++ BeyondTrustAPITester/PasswordSafeTester.cs
@@ -6,29 +6,34 @@ namespace BeyondTrustAPITester
 {
     class PasswordSafeTester
     {
-        static string USERNAME = "beyondtrust";
-        static string PASSWORD = "###";
+        static string USERNAME = "myuser";
+        static string PASSWORD = "Beyondtrust123!";
         static string APIUSER = "apiuser";
-        static string APIKEY = "###";
-        static string URL = "https://###/BeyondTrust/api/public/v3/";
+        static string APIKEY = "a48d82117ce24d6afd76d1b0c7338ceffcdd4e36e1614e2324fb90310a99b427b96f3c5c08f8889b36dd3879dbaac1a29c5a02d93ed229ce711aa976075e3d35";
+        static string URL = "https://20.119.41.120/BeyondTrust/api/public/v3/";
+        static string CERT_THUMB = "f98bbadefc0365bbf69aa0c42c84c5193fba39b5";
+        static string CERT_PASS = "Beyondtrust123";
 
         static void Main(string[] args)
         {
-            var client = new PasswordSafePAM();
+            var pam = new PasswordSafePAM();
             var initVars = new Dictionary<string, string>()
             {
                 { "Host", URL },
                 { "APIKey", APIKEY },
-                { "Username", USERNAME }
+                { "Username", USERNAME },
+                //{ "ClientCertificate", CERT_THUMB },
+                //{ "ClientCertificatePassword", CERT_PASS }
             };
 
             var instanceVars = new Dictionary<string, string>()
             {
-                { "SystemID", "1" },
-                { "AccountID", "1" }
+                { "SystemId", "3" },
+                { "AccountId", "5" }
             };
 
-            client.GetPassword(instanceVars, initVars);
+            string password = pam.GetPassword(instanceVars, initVars);
+            string s = password;
         }
     }
 }
